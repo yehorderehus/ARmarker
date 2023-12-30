ff4792c20029ef479658ac77d242287b4cc988fb
yehorderehus <yehor.derehus@student.tuke.sk>
2023-12-30 15:19:00 +0100

    Renamed

--- obj_loader.py
+++ obj_loader.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class ModelLoader:
+class ObjLoader:
     buffer = []
 
     @staticmethod
@@ -20,15 +20,15 @@ class ModelLoader:
             if i % 3 == 0:  # Sort the vertex coordinates
                 start = ind * 3
                 end = start + 3
-                ModelLoader.buffer.extend(vertices[start:end])
+                ObjLoader.buffer.extend(vertices[start:end])
             elif i % 3 == 1:  # Sort the texture coordinates
                 start = ind * 2
                 end = start + 2
-                ModelLoader.buffer.extend(textures[start:end])
+                ObjLoader.buffer.extend(textures[start:end])
             elif i % 3 == 2:  # Sort the normal vectors
                 start = ind * 3
                 end = start + 3
-                ModelLoader.buffer.extend(normals[start:end])
+                ObjLoader.buffer.extend(normals[start:end])
 
     @staticmethod  # TODO Unsorted vertex buffer for use with glDrawElements
     def create_unsorted_vertex_buffer(indices_data, vertices,
@@ -38,17 +38,17 @@ class ModelLoader:
         for i1 in range(num_verts):
             start = i1 * 3
             end = start + 3
-            ModelLoader.buffer.extend(vertices[start:end])
+            ObjLoader.buffer.extend(vertices[start:end])
 
             for i2, data in enumerate(indices_data):
                 if i2 % 3 == 0 and data == i1:
                     start = indices_data[i2 + 1] * 2
                     end = start + 2
-                    ModelLoader.buffer.extend(textures[start:end])
+                    ObjLoader.buffer.extend(textures[start:end])
 
                     start = indices_data[i2 + 2] * 3
                     end = start + 3
-                    ModelLoader.buffer.extend(normals[start:end])
+                    ObjLoader.buffer.extend(normals[start:end])
 
                     break
 
@@ -60,7 +60,7 @@ class ModelLoader:
             print(buffer[start:end])
 
     @staticmethod
-    def load_model(file, sorted=True):
+    def load_obj(file, sorted=True):
         vert_coords = []  # Has to contain the vertex coordinates
         tex_coords = []  # Has to contain the texture coordinates
         norm_coords = []  # Has to contain the vertex normals
@@ -75,18 +75,18 @@ class ModelLoader:
                 # Check if values list is not empty before indexing
                 if values:
                     if values[0] == 'v':
-                        ModelLoader.search_data(values, vert_coords,
+                        ObjLoader.search_data(values, vert_coords,
                                                 'v', 'float')
                     elif values[0] == 'vt':
-                        ModelLoader.search_data(values, tex_coords,
+                        ObjLoader.search_data(values, tex_coords,
                                                 'vt', 'float')
                     elif values[0] == 'vn':
-                        ModelLoader.search_data(values, norm_coords,
+                        ObjLoader.search_data(values, norm_coords,
                                                 'vn', 'float')
                     elif values[0] == 'f':
                         for value in values[1:]:
                             val = value.split('/')
-                            ModelLoader.search_data(val, all_indices,
+                            ObjLoader.search_data(val, all_indices,
                                                     'f', 'int')
                             indices.append(int(val[0]) - 1)
 
@@ -94,19 +94,19 @@ class ModelLoader:
 
         if sorted:
             # Use with glDrawArrays
-            ModelLoader.create_sorted_vertex_buffer(all_indices, vert_coords,
+            ObjLoader.create_sorted_vertex_buffer(all_indices, vert_coords,
                                                     tex_coords, norm_coords)
         else:
             # Use with glDrawElements
-            ModelLoader.create_unsorted_vertex_buffer(all_indices, vert_coords,
+            ObjLoader.create_unsorted_vertex_buffer(all_indices, vert_coords,
                                                       tex_coords, norm_coords)
 
         # Create a local copy of the buffer list,
         # otherwise it will overwrite the static field buffer
-        buffer = ModelLoader.buffer.copy()
+        buffer = ObjLoader.buffer.copy()
 
         # After the copy, make sure to set it back to an empty list
-        ModelLoader.buffer = []
+        ObjLoader.buffer = []
 
         return np.array(indices, dtype='uint32'), np.array(buffer,
                                                            dtype='float32')
